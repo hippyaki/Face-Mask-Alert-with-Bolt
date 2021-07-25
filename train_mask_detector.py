5997c6dfd9a3b8f300206bb9353336b24d2c7f30
Akshayan Sinha <akshayan.sinha@gmail.com>
2021-07-25 16:57:44 +0530

    Update train_mask_detector.py

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -26,7 +26,7 @@ INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
 
-DIRECTORY = r"C:\dataset"
+DIRECTORY = r"C:\dataset\dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
 # grab the list of images in our dataset directory, then initialize
